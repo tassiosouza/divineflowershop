51d4bff81202ee0f84fa83ec98903697abb92988
Tassio Souza <souza.tassio@gmail.com>
2025-06-10 21:10:34 -0700

    Add custom WooCommerce single product template

---
+++ wp-content/themes/twentytwentyfive/woocommerce/single-product.php
@@ -0,0 +1,131 @@
+<?php
+/**
+ * Custom single product template styled like product-detail.html
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+get_header( 'shop' );
+
+do_action( 'woocommerce_before_main_content' );
+
+if ( have_posts() ) :
+    while ( have_posts() ) :
+        the_post();
+        global $product;
+?>
+<section class="py-80">
+    <div class="container-fluid">
+        <div class="row justify-content-center">
+            <div class="col-xxl-8 col-xl-10 col-lg-11">
+                <div class="product-detail pb-80">
+                    <div class="row row-gap-4">
+                        <div class="col-md-6">
+                            <div class="row align-items-center row-gap-3">
+                                <div class="list">
+                                    <button class="slider-btn prev-btn" data-slide="preview-slider-nav">
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 17 16" fill="none">
+                                            <path d="M0.857543 12.1506C1.14152 12.4346 1.60203 12.4347 1.88605 12.1506L8.64436 5.39213L15.403 12.1506C15.687 12.4346 16.1475 12.4347 16.4315 12.1506C16.7155 11.8666 16.7155 11.4061 16.4315 11.1221L9.15859 3.84935C9.0222 3.71296 8.83723 3.63635 8.64436 3.63635C8.45148 3.63635 8.26647 3.71301 8.13013 3.84939L0.857592 11.1221C0.573519 11.4061 0.573519 11.8666 0.857543 12.1506Z"/>
+                                        </svg>
+                                    </button>
+                                    <div class="preview-slider-nav mt-3">
+                                        <?php
+                                        $ids = $product->get_gallery_image_ids();
+                                        array_unshift( $ids, $product->get_image_id() );
+                                        foreach ( $ids as $image_id ) : ?>
+                                            <div class="detail-img-block">
+                                                <img src="<?php echo esc_url( wp_get_attachment_image_url( $image_id, 'thumbnail' ) ); ?>" alt="">
+                                            </div>
+                                        <?php endforeach; ?>
+                                    </div>
+                                    <button class="slider-btn next-btn" data-slide="preview-slider-nav">
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 17 16" fill="none">
+                                            <path d="M16.4315 3.84935C16.1475 3.56537 15.687 3.56532 15.403 3.84939L8.6447 10.6078L1.88606 3.84935C1.60208 3.56537 1.14157 3.56532 0.857549 3.84939C0.573525 4.13342 0.573525 4.59388 0.857549 4.8779L8.13047 12.1506C8.26686 12.287 8.45183 12.3636 8.6447 12.3636C8.83757 12.3636 9.02259 12.287 9.15893 12.1506L16.4315 4.87786C16.7155 4.59388 16.7155 4.13337 16.4315 3.84935Z"/>
+                                        </svg>
+                                    </button>
+                                </div>
+                                <div class="preview">
+                                    <div class="preview-slider">
+                                        <?php foreach ( $ids as $image_id ) : ?>
+                                            <div class="detail-img-block">
+                                                <img src="<?php echo esc_url( wp_get_attachment_image_url( $image_id, 'large' ) ); ?>" alt="">
+                                            </div>
+                                        <?php endforeach; ?>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="product-detail-content">
+                                <div class="d-flex align-items-center column-gap-2 row-gap-3 mb-16">
+                                    <h4><?php the_title(); ?></h4>
+                                    <?php if ( $product->is_in_stock() ) : ?>
+                                        <p class="green-tag"><?php esc_html_e( 'In Stock', 'twentytwentyfive' ); ?></p>
+                                    <?php endif; ?>
+                                </div>
+                                <ul class="unstyled mb-24 pro-rel">
+                                    <li class="d-flex align-items-center">
+                                        <span class="rating-stars me-1"><?php echo wc_get_rating_html( $product->get_average_rating() ); ?></span>
+                                        <span class="text-decoration-underline"><?php comments_number(); ?></span>
+                                    </li>
+                                    <?php if ( $product->get_sku() ) : ?>
+                                        <li><span class="bold-text accent-dark me-1">SKU:</span><span><?php echo esc_html( $product->get_sku() ); ?></span></li>
+                                    <?php endif; ?>
+                                </ul>
+                                <div class="d-flex align-items-center column-gap-2 row-gap-3 mb-24">
+                                    <div class="price">
+                                        <?php if ( $product->is_on_sale() ) : ?>
+                                            <del class="h6 dark-gray"><?php echo wc_price( $product->get_regular_price() ); ?></del>
+                                            <h3><?php echo wc_price( $product->get_sale_price() ); ?></h3>
+                                        <?php else : ?>
+                                            <h3><?php echo wc_price( $product->get_price() ); ?></h3>
+                                        <?php endif; ?>
+                                    </div>
+                                    <?php if ( $product->is_on_sale() ) :
+                                        $regular = (float) $product->get_regular_price();
+                                        $sale = (float) $product->get_sale_price();
+                                        if ( $regular > 0 ) {
+                                            $discount = round( ( ( $regular - $sale ) / $regular ) * 100 );
+                                            ?>
+                                            <p class="red-tag"><?php echo esc_html( $discount . '% off' ); ?></p>
+                                        <?php }
+                                    endif; ?>
+                                </div>
+                                <p class="mb-24"><?php echo $product->get_short_description(); ?></p>
+                                <hr class="dash-line mb-16">
+                                <div class="action-block mb-16">
+                                    <?php woocommerce_template_single_add_to_cart(); ?>
+                                </div>
+                                <hr class="dash-line mb-24">
+                                <div class="mb-16">
+                                    <span class="bold-text accent-dark me-1">SKU:</span><span><?php echo esc_html( $product->get_sku() ); ?></span>
+                                </div>
+                                <div class="tags mb-24">
+                                    <span class="bold-text accent-dark me-1">Tag:</span><?php echo wc_get_product_tag_list( $product->get_id() ); ?>
+                                </div>
+                                <hr class="dash-line mb-16">
+                                <div class="d-flex justify-content-between align-items-center gap-2 mb-16">
+                                    <span class="bold-text accent-dark">Share:</span>
+                                    <ul class="unstyled social-icons">
+                                        <li><a href="#"><i class="fa-brands fa-facebook-f"></i></a></li>
+                                        <li><a href="#"><i class="fa-brands fa-twitter"></i></a></li>
+                                        <li><a href="#"><i class="fa-brands fa-pinterest"></i></a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="about-product">
+                    <?php do_action( 'woocommerce_after_single_product_summary' ); ?>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<?php
+    endwhile;
+endif;
+
+do_action( 'woocommerce_after_main_content' );
+get_footer( 'shop' );
